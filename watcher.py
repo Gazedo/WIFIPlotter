465d58570282e49b6698235a07f8e9dcbfbfe067
Hayden Connor <hconnor3@gmail.com>
2017-09-27 22:06:05 +0000

    Working example, ready for testing

--- watcher.py
+++ watcher.py
@@ -4,6 +4,20 @@ import serial
 import pyinotify
 import asyncio
 import os
+import time
+
+def send_to_serial(stuff1):
+    print(stuff1)
+    path = '/home/pi/Print/'
+    files = [f for f in os.listdir(path) if os.path.isfile(path + f) and (f.endswith('hpgl') or f.endswith('plt'))]
+    print(files)
+    for f in files:
+        in_file = open(path + f, 'r')
+        ser.write(in_file.encode())
+        print('found {}'.format(f))
+        in_file.close()
+        os.remove(path + f)
+        notifier.loop.stop()
 
 ser = serial.Serial()
 ser.port = '/dev/ttyUSB0'
@@ -16,20 +30,16 @@ ser.rtscts = False
 ser.dsrdtr = False
 
 wm = pyinotify.WatchManager()
-notifier = pyinotify.Notifier(wm)
-wm.add_watch('/home/pi/Print', pyinotify.ALL_EVENTS)
+loop = asyncio.get_event_loop()
+notifier = pyinotify.AsyncioNotifier(wm, loop, callback=send_to_serial)
+wm.add_watch('/home/pi/Print', pyinotify.IN_CLOSE_WRITE)
+print('Added watch')
+loop.run_forever()
+notifier.stop()
 
-def send_to_serial(stuff1, stuff2):
-    path = '/home/pi/Print'
-    files = [f for f in os.listdir(path) if os.path.isfile(path + f) and (f.endswith('hpgl') or f.endswith('plt'))]
-    for f in files:
-        in_file = open(f, 'r')
-        ser.write(in_file.encode())
-        in_file.close()
-        os.remove(path + f)
-
-try:
-    notifier.loop(daemonize=True, callback=send_to_serial, pid_file='/tmp/pyinotify.pid', stdout='/tmp/pyinotify.log')
-except pyinotify.NotifierError, err:
-    print >> sys.stderr, err
+# notifier.loop(daemonize=True, callback=send_to_serial, pid_file='/tmp/pyinotify.pid', stdout='/tmp/pyinotify.log')
+# while True:
+    # notifier.loop(callback=send_to_serial, stdout='/tmp/watcher.log')
+    # time.sleep(10)
+    # print('Setup up notifier')
 
